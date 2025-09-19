ecc12c6068924a294c137ce2aff72b3b53cb88b8
Puli Venkata Ganesh <ganeshyadav.puli9010@gmail.com>
2025-09-19 23:01:25 +0530

    again deleted

--- api/index.js
+++
@@ -1,7 +0,0 @@
-import app from "../app.js";
-
-// Express apps are (req, res) handlers.
-// Export a function for clarity.
-export default function handler(req, res) {
-  return app(req, res);
-}
--- utils/excel.js
+++
@@ -1,7 +0,0 @@
-import XLSX from "xlsx";
-
-export function parseExcelBufferToJson(buffer) {
-  const wb = XLSX.read(buffer, { type: "buffer" });
-  const ws = wb.Sheets[wb.SheetNames[0]];
-  return XLSX.utils.sheet_to_json(ws, { defval: "" });
-}
