d3434cd8acdbb4ec5481f34235dcae704a46ec87
Puli Venkata Ganesh <ganeshyadav.puli9010@gmail.com>
2025-09-19 22:39:18 +0530

    all deleted

--- server.js
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv";
-import app from "./src/app.js";
-dotenv.config();
-
-const port = process.env.PORT || 4000;
-app.listen(port, () => {
-  console.log("Server listening on", port);
-});
--- src/utils/excel.js
+++
@@ -1,7 +0,0 @@
-export async function parseExcelBufferToJson(buffer) {
-  // Load xlsx only when we actually need to parse a file
-  const { default: XLSX } = await import("xlsx");
-  const wb = XLSX.read(buffer, { type: "buffer" });
-  const ws = wb.Sheets[wb.SheetNames[0]];
-  return XLSX.utils.sheet_to_json(ws, { defval: "" });
-}
