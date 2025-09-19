f7ac8084ee2b05bcd92f48df037715ee11beb148
Puli Venkata Ganesh <ganeshyadav.puli9010@gmail.com>
2025-09-19 23:12:42 +0530

    step 1

--- app.js
+++ app.js
@@ -10,31 +10,9 @@ app.use(cors({
 
 app.use(express.json());
 
-app.use('/api/upload', require('./routes/uploadRoutes'));
-app.use('/api/products', require('./routes/productRoutes'));
-app.use('/api/b2b-customers', require('./routes/b2bCustomerRoutes'));
-app.use('/api/b2c-customers', require('./routes/b2cCustomerRoutes'));
-app.use('/api/signup', require('./routes/b2cCustomerRoutes'));
-app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api/wishlist', require('./routes/wishlistRoutes'));
-app.use('/api/cart', require('./routes/cartRoutes'));
-app.use('/api/user', require('./routes/userRoutes'));
-
-app.get('/', (req, res) => res.status(200).send('Taras Kart API'));
+app.get('/', (req, res) => res.status(200).send('CHV API'));
 app.get('/healthz', (req, res) => res.status(200).send('ok'));
 
 app.use((req, res) => res.status(404).send('Not found'));
-
-
-app.get('/api/debug/blob-env', (req, res) => {
-  res.json({
-    hasToken: Boolean(
-      process.env.BLOB_READ_WRITE_TOKEN ||
-      process.env.VERCEL_BLOB_READ_WRITE_TOKEN ||
-      process.env.VERCEL_BLOB_RW_TOKEN
-    )
-  });
-});
-
 
 module.exports = app;
--- db.js
+++ db.js
@@ -1,24 +1,9 @@
-/*const { Pool } = require('pg');
-
-const pool = new Pool({
-  host: 'localhost',
-  user: 'postgres',
-  password: '9010@Gane',
-  database: 'postgres',
-  port: 5432
-});
-
-module.exports = pool; */
-
 require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false  
-  }
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
-
